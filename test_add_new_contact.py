fd05eb9f14871d15b468a37f0811852857d29631
Elena Prokorym <111298569+ElenaProkorym@users.noreply.github.com>
2023-02-18 21:10:25 -0600

    Task #4: Transfer tests from unittest to py.test and implement fixtures

--- test_add_new_contact.py
+++ test_add_new_contact.py
@@ -1,108 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-import unittest
 from contact_information import Contactinfo
+from contact_app import ContactApplication
+import pytest
 
-class TestAddNewContact(unittest.TestCase):
-    def setUp(self):
-        firefox_binary = FirefoxBinary()
-        self.wd = webdriver.Firefox(firefox_binary=firefox_binary)
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def capp(request):
+    fixture = ContactApplication()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_new_empty_contact(self):
-        wd = self.wd
-        # Open home page
-        self.open_home_page(wd)
-        # Login
-        self.login(wd, username = "admin", password = "secret")
-        # Add new contact
-        wd.find_element_by_link_text("add new").click()
-        self.fill_new_contact_form(wd, Contactinfo(address = "", company = "", firstname = "", home = "", lastname = ""))
-        self.return_to_home_page(wd)
-        self.logout(wd)
+def test_add_new_empty_contact(capp):
+    capp.login(username = "admin", password = "secret")
+    capp.fill_new_contact_form(Contactinfo(address = "", company = "", firstname = "", home = "", lastname = ""))
+    capp.logout()
 
-    def test_add_new_contact(self):
-        wd = self.wd
-        # Open home page
-        self.open_home_page(wd)
-        # Login
-        self.login(wd, username = "admin", password = "secret")
-        # Add new contact
-        wd.find_element_by_link_text("add new").click()
-        self.fill_new_contact_form(wd, Contactinfo(address = "Минск, Беларусь", company = "ZippyBus", firstname = "Елена", home ="224616", lastname = "Прокорым"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
+def test_add_new_contact(capp):
+    capp.login(username = "admin", password = "secret")
+    capp.fill_new_contact_form(Contactinfo(address = "Минск, Беларусь", company = "ZippyBus", firstname = "Елена", home ="224616", lastname = "Прокорым"))
+    capp.logout()
 
-    def logout(self, wd):
-        # Logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_home_page(self, wd):
-        # Return to home page
-        wd.find_element_by_link_text("home page").click()
-
-    def fill_new_contact_form(self, wd, contactinfo):
-        # Fill contact form
-        # First name
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contactinfo.firstname)
-        # Last name
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contactinfo.lastname)
-        # fill company
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contactinfo.company)
-        # fill address
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contactinfo.address)
-        # fill telephone
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contactinfo.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("email").click()
-        # Birthday
-        wd.find_element_by_name("bday").click()
-        wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("aday").click()
-        wd.find_element_by_name("aday").click()
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
