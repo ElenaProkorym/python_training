7bf2e77e8f14db2ec2215c328c962a1e24accd58
Elena Prokorym <111298569+ElenaProkorym@users.noreply.github.com>
2023-02-19 19:33:19 -0600

    Improvement Task #4: "Transfer tests from unittest to py.test and implement fixtures"

--- test_add_new_contact.py
+++ test_add_new_contact.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 from contact_information import Contactinfo
-from contact_app import ContactApplication
+from application import Application
 import pytest
 
 @pytest.fixture
-def capp(request):
-    fixture = ContactApplication()
+def app(request):
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
-def test_add_new_empty_contact(capp):
-    capp.login(username = "admin", password = "secret")
-    capp.fill_new_contact_form(Contactinfo(address = "", company = "", firstname = "", home = "", lastname = ""))
-    capp.logout()
+def test_add_new_empty_contact(app):
+    app.login(username = "admin", password = "secret")
+    app.fill_new_contact_form(Contactinfo(address = "", company = "", firstname = "", home = "", lastname = ""))
+    app.logout()
 
-def test_add_new_contact(capp):
-    capp.login(username = "admin", password = "secret")
-    capp.fill_new_contact_form(Contactinfo(address = "Минск, Беларусь", company = "ZippyBus", firstname = "Елена", home ="224616", lastname = "Прокорым"))
-    capp.logout()
+def test_add_new_contact(app):
+    app.login(username = "admin", password = "secret")
+    app.fill_new_contact_form(Contactinfo(address = "Минск, Беларусь", company = "ZippyBus", firstname = "Елена", home ="224616", lastname = "Прокорым"))
+    app.logout()
 
