2205feb38e5fc2d61fb2d876c833e66ff4fed30c
Elena Prokorym <111298569+ElenaProkorym@users.noreply.github.com>
2023-02-21 13:59:12 -0600

    Made a common fixture for all tests in the file "conftest".

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from fixture.application import Application
-import pytest
 
-@pytest.fixture
-def helper(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
-def test_add_group(helper):
-    helper.session.login(username ="admin", password ="secret")
-    helper.group.create(Group(name ="group1", header ="Feb", footer ="New group created"))
-    helper.session.logout()
+def test_add_group(app):
+    app.session.login(username ="admin", password ="secret")
+    app.group.create(Group(name ="group1", header ="Feb", footer ="New group created"))
+    app.session.logout()
 
-def test_add_empty_group(helper):
-    helper.session.login(username ="admin", password ="secret")
-    helper.group.create(Group(name ="", header ="", footer =""))
-    helper.session.logout()
+def test_add_empty_group(app):
+    app.session.login(username ="admin", password ="secret")
+    app.group.create(Group(name ="", header ="", footer =""))
+    app.session.logout()
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 from model.contact_information import Contactinfo
-from fixture.application import Application
-import pytest
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_new_empty_contact(app):
     app.session.login(username = "admin", password = "secret")
