fc92ef7ebac7c28c4d91e8dd2ffaf52a46528045
Elena Prokorym <111298569+ElenaProkorym@users.noreply.github.com>
2023-02-21 20:09:44 -0600

    Created new tests related to group (delete and edit group).

--- fixture/group.py
+++ fixture/group.py
@@ -24,3 +24,36 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit editions
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit update
+        wd.find_element_by_name("update").click()
+
+    def open_groups_page(self):
+        wd = self.app.wd
+        wd.get("http://localhost/addressbook/group.php")
---
+++ test/test_edit_group.py
@@ -0,0 +1,5 @@
+from model.group import Group
+def test_edit_first_group(app):
+    app.session.login(username ="admin", password ="secret")
+    app.group.edit_first_group(Group(name ="1", header ="Jan", footer ="Edit to group"))
+    app.session.logout()
