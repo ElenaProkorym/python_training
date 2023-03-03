b89ebfcacf09c5f4fd0b9b491406a9f7fcbc6983
Elena Prokorym <111298569+ElenaProkorym@users.noreply.github.com>
2023-03-02 20:15:32 -0600

    Made a change to test_del_contact.

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,7 +2,7 @@ from model.contact_information import ContactInfo
 def test_delete_first_contact(app):
     app.open_home_page()
     if app.contact.count() == 0:
-        app.contact.fill_new_form(ContactInfo(firstname ="test",address ="", company ="", home ="", lastname =""))
+        app.contact.fill_new_form(ContactInfo())
         app.open_home_page()
     app.contact.select_first_contact()
     app.contact.click_delete_first_contact()
