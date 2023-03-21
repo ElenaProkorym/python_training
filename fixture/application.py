f289ebf2f2571801a286fc7c6e09796617a6b6fd
Elena Prokorym <111298569+ElenaProkorym@users.noreply.github.com>
2023-03-20 21:51:02 -0500

    Implemented command line options that allow you to specify the type of browser and the address of the application under test when running tests. Exercise (16).

--- fixture/application.py
+++ fixture/application.py
@@ -6,13 +6,22 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 class Application:
-    def __init__(self):
-        firefox_binary = FirefoxBinary()
-        self.wd = webdriver.Firefox(firefox_binary=firefox_binary)
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            firefox_binary = FirefoxBinary()
+            self.wd = webdriver.Firefox(firefox_binary=firefox_binary)
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        #self.wd = webdriver.Firefox(firefox_binary=firefox_binary)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,7 +33,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/")):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def is_element_present(self, how, what):
         try:
