9143fd4aa2200f74cb13724c07fa105014f46bab
Elena Prokorym <111298569+ElenaProkorym@users.noreply.github.com>
2023-04-12 11:20:30 -0500

    Compare the contact information on the main page with the information loaded from the database.Exercise (21).

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,13 +1,37 @@
 #import pymysql.cursors
 import mysql.connector
+from fixture.db import DbFixture
+from fixture.orm import ORMFixture
 
-connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+#db= ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db= DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    cursor=connection.cursor()
-    cursor.execute("select * from addressbook")
-    for row in cursor.fetchall():
-        print(row)
+    # l = db.get_contact_list()
+    # for item in l:
+    #     print(item)
+    # print(len(l))
+
+    contacts= db.get_contact_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
+
+    #FOR GROUPS
+    # groups=db.get_group_list()
+    # for group in groups:
+    #     print(group)
+    # print(len(groups))
 
 finally:
-    connection.close()
+    db.destroy()
+#connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+
+# try:
+#     cursor=connection.cursor()
+#     cursor.execute("select * from addressbook")
+#     for row in cursor.fetchall():
+#         print(row)
+#
+# finally:
+#     connection.close()
--- test/test_check_contact_info.py
+++ test/test_check_contact_info.py
@@ -1,5 +1,13 @@
 import re
 from turtle import clear
+from model.contact_information import ContactInfo
+
+
+def test_contact_info_on_db(app, db):
+    contacts_from_home_page = app.contact.get_contact_list()
+    contacts_from_db = db.get_contact_list()
+    assert sorted(contacts_from_home_page, key=ContactInfo.id_or_max) == sorted(contacts_from_db,key=ContactInfo.id_or_max)
+
 
 def test_contact_info_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
@@ -14,6 +22,14 @@ def test_contact_info_on_home_page(app):
 def clear(s):
     return re.sub("[() -]","", s)
 
+
+# def merge_phones_like_on_db(db):
+#     # return merge([contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone])
+#     return "\n".join(filter(lambda x: x != "",
+#                             map(lambda x: clear(x),
+#                                 filter(lambda x: x is not None,
+#                                        [db.homephone, db.mobilephone, db.workphone,
+#                                         db.phone2]))))
 
 def merge_email_address_info_like_on_home_page(contact):
     #return merge([contact.email, contact.email2, contact.email3])
